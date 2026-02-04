6244fd20a856e885b5760a8e63d138a60f3900dd
06-month <ta4189@kakao.com>
2026-02-04 10:26:07 +0900

    Fix build: add CSS and ImportMeta type declarations

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
-/// <reference types="vite/client" />
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly BASE_URL: string;
+  readonly MODE: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  readonly SSR: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
